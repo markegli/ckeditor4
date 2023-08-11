5bf8db1fb99aa8ab79ad62ce81fd20b32410e73d
Youssef Attia <16466991+youssef-attia@users.noreply.github.com>
2023-08-11 17:01:42 +0000

    [Trusted Types] Fixing violations sourced from setAttribute wrapper function (#3)
    * setAttribute tt violations
    
    * change to legacy conversion
    
    * const to var

--- core/scriptloader.js
+++ core/scriptloader.js
@@ -114,10 +114,26 @@ CKEDITOR.scriptLoader = ( function() {
 
 					// Create the <script> element.
 					var script = new CKEDITOR.dom.element( 'script' );
-					script.setAttributes( {
-						type: 'text/javascript',
-						src: url
-					} );
+					if (self.trustedTypes && self.trustedTypes.createPolicy) {
+						// Legacy converted for now because of the possibility of using alternate CDNs for serving scripts.
+						var policy = self.trustedTypes.createPolicy(
+						  'scriptloader#loadScript',
+						  {
+							createScriptURL: function (url) {
+								return url;
+							},
+						  }
+						);
+						script.setAttributes( {
+							type: 'text/javascript',
+							src: policy.createScriptURL(url)
+						} );
+					  } else {
+						script.setAttributes( {
+							type: 'text/javascript',
+							src: url
+						} );
+					  }
 
 					if ( callback ) {
 						// The onload or onerror event does not fire in IE8 and IE9 Quirks Mode (https://dev.ckeditor.com/ticket/14849).
